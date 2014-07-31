b79b7d8b154fd87c3baca51458d7f0329fe218c6
feldmannst <feldmannst@gmail.com>
2014-07-30 22:38:21 -0400

    FixFontBug

--- Catch22/App_Start/BundleConfig.cs
+++ Catch22/App_Start/BundleConfig.cs
@@ -27,12 +27,12 @@ namespace Catch22
                       "~/Scripts/swipe.js",
                       "~/Scripts/mainPage.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/css").Include(
+            bundles.Add(new StyleBundle("~/Content/cssMain").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/css/font-awesome.css",
                       "~/Content/Site.css"));
 
-            bundles.Add(new StyleBundle("~/bundles/cssCMS").Include(
+            bundles.Add(new StyleBundle("~/Content/cssCMS").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/css/font-awesome.css",
                       "~/Content/themes/base/jquery.ui.all.css",
